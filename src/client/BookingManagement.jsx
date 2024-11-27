eeed3c4c853e6a24630a31cadb28a768be4ed3ab
Zenja222 <143327280+Zenja222@users.noreply.github.com>
2024-11-27 13:11:31 +0200

    commit

--- src/client/BookingManagement.jsx
+++ src/client/BookingManagement.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAll = async () => {
     try {
         const response =
-            await axios.get('http://localhost:8080/glamping');
+            await axios.get('http://https://cld5-193-40-147-101.ngrok-free.app/glamping');
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -22,7 +22,7 @@ export const getAll = async () => {
 export const getGlampingById = async (id) => {
     try {
         const response =
-            await axios.get(`http://localhost:8080/glamping/${id}`);
+            await axios.get(`http://https://cld5-193-40-147-101.ngrok-free.app/glamping/${id}`);
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -41,7 +41,7 @@ export const getGlampingById = async (id) => {
 export const filterGlamping = async () => {
     try {
         const response =
-            await axios.get(`http://localhost:8080/glamping/filter`);
+            await axios.get(`http://https://cld5-193-40-147-101.ngrok-free.app/glamping/filter`);
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -60,7 +60,7 @@ export const filterGlamping = async () => {
 export const addRating = async (id, rating) => {
     try {
         const response =
-            await axios.post(`http://localhost:8080/reviews/${id}`, null, {
+            await axios.post(`http://https://cld5-193-40-147-101.ngrok-free.app/reviews/${id}`, null, {
                 params: {
                     rating: rating
                 }
@@ -84,7 +84,7 @@ export const addRating = async (id, rating) => {
 export const getAverageRating = async (id) => {
     try {
         const response =
-            await axios.get(`http://localhost:8080/reviews/average/${id}`);
+            await axios.get(`http://https://cld5-193-40-147-101.ngrok-free.app/reviews/average/${id}`);
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -103,7 +103,7 @@ export const getAverageRating = async (id) => {
 
 export const filterByField = async (sortField, sortDirection) => {
     try {
-        const response = await axios.get(`http://localhost:8080/glamping/filter`, {
+        const response = await axios.get(`http://https://cld5-193-40-147-101.ngrok-free.app/glamping/filter`, {
             params: {sortField, sortDirection}
         });
         return response.data; // This will return the filtered glamping data
@@ -116,7 +116,7 @@ export const filterByField = async (sortField, sortDirection) => {
 export const updateGlamping = async (id,glamping) => {
     try {
         const response =
-            await axios.put(`http://localhost:8080/glamping/${id}`, glamping);
+            await axios.put(`http://https://cld5-193-40-147-101.ngrok-free.app/glamping/${id}`, glamping);
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -136,7 +136,7 @@ export const updateGlamping = async (id,glamping) => {
 export const addGlamping = async (glamping) => {
     try {
         const response =
-            await axios.post('http://localhost:8080/glamping', glamping);
+            await axios.post('http://https://cld5-193-40-147-101.ngrok-free.app/glamping', glamping);
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -155,7 +155,7 @@ export const addGlamping = async (glamping) => {
 export const searchByName = async (keyword,language) => {
     try {
         const response =
-            await axios.get('http://localhost:8080/glamping/search', {params: {keyword, language}});
+            await axios.get('http://https://cld5-193-40-147-101.ngrok-free.app/glamping/search', {params: {keyword, language}});
         return response.data;
     } catch (error) {
         if (error.response) {
@@ -173,7 +173,7 @@ export const searchByName = async (keyword,language) => {
 }
 export const filterByPriceRange = async (minPrice, maxPrice) => {
     try {
-        const response = await axios.get('http://localhost:8080/glamping/filterByPrice', {
+        const response = await axios.get('http://https://cld5-193-40-147-101.ngrok-free.app/glamping/filterByPrice', {
             params: { minPrice, maxPrice }
         });
         return response.data;
